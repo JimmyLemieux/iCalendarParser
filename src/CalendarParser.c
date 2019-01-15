378a16b15247bab149cd4fa91b64a0a0f65e1117
James Lemieux <jimmylemieux9@gmail.com>
2019-01-15 01:31:32 -0500

    lol

--- src/CalendarParser.c
+++ src/CalendarParser.c
@@ -264,6 +264,10 @@ ICalErrorCode createCalendar(char* fileName, Calendar** obj) {
 
     //validateFileLines(test,arraySize,fileLines); // Validation of the lines in the file and the tokenizer
 
+    for(i = 0;i<arraySize;i++) {
+        printf("%s",test[i]);
+    }
+
     free_fields(test,arraySize);
     free(tempFile);
     free(fileExtension);
