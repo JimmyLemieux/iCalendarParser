e7a1162ad25be54ce2766c606972eece395dd62a
James Lemieux <jimmylemieux9@gmail.com>
2019-01-14 23:33:07 -0500

    Fixed Frees

--- src/CalendarParser.c
+++ src/CalendarParser.c
@@ -47,35 +47,36 @@ char ** init_str_array(int N, int K) {
 }
 
 
-void  splitLine(char *fileFeed,char* del, int *arraySize) {
-    char *token;
-    char *tempLine;
-    char **fileLines;
-    int j;
-    if(fileFeed == NULL || del == NULL) {
-        return;
-    }
+// void  splitLine(char *fileFeed,char* del, int *arraySize) {
+//     char *token;
+//     char *tempLine;
+//     char **fileLines;
+//     int j;
+//     if(fileFeed == NULL || del == NULL) {
+//         return;
+//     }
 
 
-    token = (char *)malloc(sizeof(char) * 256);
-    token = strtok(fileFeed, del);
-    j = 0;
-    while(token != NULL) {
-        if(j % 255 == 0 && j != 0) {
-            fileLines = (char **)realloc(fileLines, sizeof(char *)* 255);
-        }
-        strcat(token,"\0");
-        tempLine = (char *)malloc(strlen(token)+1);
-        strcpy(tempLine,token); //includes the null ter from token
-        fileLines[j] = (char *)malloc(strlen(tempLine) + 1);
-        strcpy(fileLines[j],tempLine);
-        token = strtok(NULL,del); 
-        j++;
-    }
-    *arraySize = j;
-    free(tempLine);
-    tempLine = NULL;
-}
+//     token = (char *)malloc(sizeof(char) * 256);
+//     token = strtok(fileFeed, del);
+//     j = 0;
+//     while(token != NULL) {
+//         if(j % 255 == 0 && j != 0) {
+//             fileLines = (char **)realloc(fileLines, sizeof(char *)* 255);
+//         }
+//         strcat(token,"\0");
+//         tempLine = (char *)malloc(strlen(token)+1);
+//         strcpy(tempLine,token); //includes the null ter from token
+//         fileLines[j] = (char *)malloc(strlen(tempLine) + 1);
+//         strcpy(fileLines[j],tempLine);
+//         token = strtok(NULL,del); 
+//         j++;
+//     }
+//     *arraySize = j;
+//     free(tempLine);
+//     tempLine = NULL;
+//     free(token);
+// }
 
 /*Make a trim that will remove white space,
 If the line has a space at the begin and is a potential line fold then do a special case for the trim */
@@ -153,6 +154,7 @@ char ** readFileChar(char *fileName, int *arraySize,int *fileLines) {
     *fileLines = fileLineCount;
     *arraySize = lineSize;
     fclose(file);
+    free(stringBuffer);
     return lines;
 }
 
@@ -203,8 +205,8 @@ ICalErrorCode createCalendar(char* fileName, Calendar** obj) {
     int fileLines;
     char *stringBuffer = calloc(1,sizeof(stringBuffer));
 
-    obj = malloc(sizeof(Calendar*));
-    *obj = malloc(sizeof(Calendar));
+    // obj = malloc(sizeof(Calendar*));
+    // *obj = malloc(sizeof(Calendar));
     
 
     if(fileName == NULL) {
@@ -266,8 +268,12 @@ ICalErrorCode createCalendar(char* fileName, Calendar** obj) {
 
 
 
-
-
+    //Free
+    for(i = 0;i<arraySize;i++) {
+        free(test[i]);
+    }
+    free(test);
+    free(stringBuffer);
     free(tempFile);
     free(fileExtension);
     return OK;
