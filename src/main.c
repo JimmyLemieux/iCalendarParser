ef772f8e8010a2e9788912c530c2212e39349d25
James Lemieux <jimmylemieux9@gmail.com>
2019-01-27 20:46:47 -0500

    Test

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main() {
     ICalErrorCode error;
     char * errorStr = NULL;
     char *outString;
-    error = createCalendar("test.ics", &obj);
+    error = createCalendar("qqq.ics", &obj);
 
     errorStr = printError(error);
 
