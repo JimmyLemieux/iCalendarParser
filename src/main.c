3b4ff21b74afb25c1e83b90bf33d80fc107e4941
James Lemieux <jimmylemieux9@gmail.com>
2019-01-29 00:43:28 -0500

    seg fault fix

--- src/main.c
+++ src/main.c
@@ -6,12 +6,9 @@
 /* I am going to implement the dateTime helper functions */
 
 int main(int argc, char **argv) {
-
     Calendar *obj;
     argc != 2 ? printError(createCalendar("sample1.ics",&obj)) : printError(createCalendar(argv[1],&obj));
-
-    printCalendar(obj);
-    
+    if(obj != NULL)printCalendar(obj);
     deleteCalendar(obj);
     return 0;
 }
