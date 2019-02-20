68f56450067ccc4d58b370d2db820ace9b8f43d7
James Lemieux <jimmylemieux9@gmail.com>
2019-02-19 20:34:30 -0500

    tet

--- A2/src/CalendarParser.c
+++ A2/src/CalendarParser.c
@@ -1695,15 +1695,15 @@ ICalErrorCode fetchCalEvents(Calendar *obj, char **lines,int arraySize) {
                 }
             } else {
                 newEventProp = malloc(sizeof(Property) * strlen(right) + 200);
-                // if(isEmpty(right) || isEmpty(left)) {
-                //     deallocator(left);
-                //     deallocator(right);
-                //     freeList(alarmList);
-                //     freeList(eventPropList);
-                //     free(newEventProp);
-                //     free(new_event);
-                //     return INV_EVENT;
-                // }
+                if(isEmpty(right) || isEmpty(left)) {
+                    deallocator(left);
+                    deallocator(right);
+                    freeList(alarmList);
+                    freeList(eventPropList);
+                    free(newEventProp);
+                    free(new_event);
+                    return INV_EVENT;
+                }
                 if(!isEmpty(left))strcpy(newEventProp->propName, left);
                 if(!isEmpty(right))strcpy(newEventProp->propDescr,right);
                 insertBack(eventPropList,newEventProp);
