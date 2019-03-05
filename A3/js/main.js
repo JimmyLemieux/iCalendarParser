47dcf1b43415faa6013fa6c03da933c353a8c1ad
James Lemieux <jimmylemieux9@gmail.com>
2019-03-04 21:08:52 -0500

    logo change

---
+++ A3/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+
+    //This is to make the logo increase
+    var homeImage = $("#home-img");
+    var ht = homeImage.height();
+    var wd = homeImage.width();
+    var mult = 1.4;
+
+    homeImage.on("mouseenter", function() {
+        $(this).animate({height: ht*mult, width: wd*mult}, 100); 
+    });
+
+    homeImage.on("mouseleave", function() {
+        $(this).animate({height: ht, width: wd}, 100); 
+    });
+
+
+});
