306c465bb0d1c406c1ab0b4524324101e2fa6382
James Lemieux <jimmylemieux9@gmail.com>
2019-03-05 13:13:38 -0500

    jquery changes

--- A3/js/main.js
+++ A3/js/main.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
     //This is to make the logo increase
     var homeImage = $("#home-img");
+    var navObjs = [];
+
+
+    $(".col").find("#nav-item").each(function() {
+        navObjs.push({
+            objRef: $(this),
+            width: $(this).width(),
+            height: $(this).height()
+        });
+    });
+
+    navObjs.forEach(element => {
+        console.log(element.width + " " + element.height);
+        var navItemObj = element.objRef;
+        navItemObj.on("mouseenter", function() {
+            $(this).animate({fontSize: 25}, 100);
+        });
+        navItemObj.on("mouseleave", function() {
+            $(this).animate({fontSize: 20}, 100);
+        });
+    });
+
     var ht = homeImage.height();
     var wd = homeImage.width();
     var mult = 1.4;
