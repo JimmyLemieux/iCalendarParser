a642c5704f754abd4ad45443c43e584fc921bded
James Lemieux <jimmylemieux9@gmail.com>
2019-03-17 19:30:11 -0400

    dir change

--- app.js
+++ app.js
@@ -1,103 +1,120 @@
- 'use strict'
+'use strict'
 
- const ffi = require("ffi"); //This is so the C functions can connect to our library
+// C library API
+const ffi = require('ffi');
 
- const express = require("express"); // Express library
- const app = express();
+// Express App (Routes)
+const express = require("express");
+const app     = express();
+const path    = require("path");
+const fileUpload = require('express-fileupload');
 
- const path = require("path"); //This is the path utility library
- const fileUpload = require("express-fileupload"); //File upload library
+app.use(fileUpload());
 
- app.use(fileUpload());
+// Minimization
+const fs = require('fs');
+const JavaScriptObfuscator = require('javascript-obfuscator');
+
+// Important, pass in port as in `npm run dev 1234`, do not change
+const portNum = process.argv[2];
+
+// Send HTML at root, do not change
+
  app.use(express.static(path.join(__dirname, 'public')));
+
  app.use(express.static(path.join(__dirname, 'assets')));
+
  app.use(express.static(path.join(__dirname, 'uploads')));
-//File reading and manipulating enviornment
- const fs = require("fs");
-
- //JavaScript obfucator
- const JavaScriptObfuscator = require("javascript-obfuscator");
-
- // Important, pass in port as in `npm run dev 1234`, do not change
- const portNum = process.argv[2];
 
 
+let sharedLib = ffi.Library('./libcal.dylib', {
+  'makeObj' : ['string', ['string']],
+  'eventJSONWrapper' : ['string', ['string']]   
+});
 
- let sharedLib = ffi.Library('./libcal', {
-    'makeObj' : ['string', ['string']]
- });
 
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+// Send Style, do not change
+app.get('/style.css',function(req,res){
+  //Feel free to change the contents of style.css to prettify your Web app
+  res.sendFile(path.join(__dirname+'/public/style.css'));
+});
+
+// Send obfuscated JS, do not change
+app.get('/index.js',function(req,res){
+  fs.readFile(path.join(__dirname+'/public/index.js'), 'utf8', function(err, contents) {
+    const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {compact: true, controlFlowFlattening: true});
+    res.contentType('application/javascript');
+    res.send(minimizedContents._obfuscatedCode);
+  });
+});
+
+//Respond to POST requests that upload files to uploads/ directory
+app.post('/upload', function(req, res) {
+  if(!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+ 
+  let uploadFile = req.files.uploadFile;
+ 
+  // Use the mv() method to place the file somewhere on your server
+  uploadFile.mv('uploads/' + uploadFile.name, function(err) {
+    if(err) {
+      return res.status(500).send(err);
+    }
+
+    res.redirect('/');
+  });
+});
+
+
+
+//Respond to GET requests for files in the uploads/ directory
+app.get('/uploads/:name', function(req , res){
+  fs.stat('uploads/' + req.params.name, function(err, stat) {
+    console.log(err);
+    if(err == null) {
+      res.sendFile(path.join(__dirname+'/uploads/' + req.params.name));
+    } else {
+      res.send('');
+    }
+  });
+});
+
+//******************** Your code goes here ********************
 
 
 
 app.get('/obj', function(req,res) {
-    let files = fs.readdirSync('./uploads/');
-
-    var arr = [];
-
-    for(var i =0;i<file.length;i++) {
-      arr[i] = sharedLib.makeObj(files[i]);
-    }
-
-    res.send(arr);
+   let files = fs.readdirSync('./uploads/');
+   var arr = [];
+   for(var i =0;i<files.length;i++) {
+     arr[i] = sharedLib.makeObj(files[i]);
+   }
+   //Sending the json from server to client side of the application
+   res.send(arr);
 });
 
- // Obfuscating the javascript code
- // Send obfuscated JS, do not change
+app.get('/eventList', function(req, res) {
+  let files = fs.readdirSync('./uploads/');
+  let arr = [];
+  for(var i = 0;i<files.length;i++) {
+    arr[i] = sharedLib.eventJSONWrapper(files[i]);
+  }
+  res.send(arr);
 
- //As of right now this is not working too proper, I think it has something to do with the file paths or something like that
- //There isn't that much that is going on here
-app.get('/main.js',function(req,res){
-   fs.readFile(path.join(__dirname+'/public/main.js'), 'utf8', function(err, contents) {
-      console.log(__dirname + '/public/main.js');
-     const minimizedContents = JavaScriptObfuscator.obfuscate(contents, {compact: true, controlFlowFlattening: true});
-     res.contentType('application/javascript');
-     res.send(minimizedContents._obfuscatedCode);
-   });
-   console.log("This should return obfuscated javscript!");
- });
+});
 
 
+//Sample endpoint
+app.get('/someendpoint', function(req , res){
+  res.send({
+    foo: "bar"
+  });
+});
 
- //Respond to POST requests that upload files to uploads/ directory
- app.post('/upload', function(req, res) {
-    console.log("The file was uploaded");
-   if(!req.files) {
-     return res.status(400).send('No files were uploaded.');
-   }
-
-   let uploadFile = req.files.someUpload;
-   // Use the mv() method to place the file somewhere on your server
-   uploadFile.mv('uploads/' + uploadFile.name, function(err) { // This will move the file to the server
-     if(err) {
-       return res.status(500).send(err);
-     }
-     res.redirect('/');
-   });
- });
-
- //Respond to GET requests for files in the uploads/ directory
- app.get('/uploads/:name', function(req , res){
-   fs.stat('uploads/' + req.params.name, function(err, stat) {
-     console.log(err);
-     if(err == null) {
-       res.sendFile(path.join(__dirname+'/uploads/' + req.params.name)); // This is actually downloading the file
-     } else {
-       res.send(''); //If there is an error
-     }
-   });
- });
-
-
-
- app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
- });
-
- app.get("/something", function(req, res) {
-   res.send("This is an end point");
- });
-
- app.listen(portNum, function(error) {
-    console.log("Running on PORT 32629!");
- });
+app.listen(portNum);
+console.log('Running app at localhost: ' + portNum);
