4e788cbc987d468d4fd058967e5929b7ebf30f53
James Lemieux <jimmylemieux9@gmail.com>
2019-03-19 22:59:36 -0400

    600 COMMITSnpm run dev 32629

--- public/main.js
+++ public/main.js
@@ -33,14 +33,14 @@ $(document).ready(function() {
         $(this).animate({height: ht, width: wd}, 100); 
     });
 
-    $.ajax({url: "http://localhost:32629/obj",
+    $.ajax({url: "obj",
         dataType: 'json',
         async: false,
         success: function(data) {
             for(var i = 0;i<data.length;i++) {
                 var retData = JSON.parse(data[i]);
                 if(!retData["isValid"]) continue;
-                $("#file-table-contents").append("<tr><th scope=\"row\"><a href=\"http://localhost:32629/uploads/" + retData["fileName"] + "\">" + retData["fileName"] + "</a></th><td>"+ retData["version"] + "</td><td>" + retData['prodID'] + "</td><td>" +retData['numProps']+"</td><td>" + retData['numEvents'] + "</td></tr>");
+                $("#file-table-contents").append("<tr><th scope=\"row\"><a href=\"uploads/" + retData["fileName"] + "\">" + retData["fileName"] + "</a></th><td>"+ retData["version"] + "</td><td>" + retData['prodID'] + "</td><td>" +retData['numProps']+"</td><td>" + retData['numEvents'] + "</td></tr>");
                 $(".dropdown-menu").append("<p class=\"item\">" + retData["fileName"] + "</p>");
             }
         }
@@ -52,7 +52,7 @@ $(document).ready(function() {
             var newTitle = $(this).text();
             //We are going to parse the event list contents and then put them into the table
             // Make an ajax call to our parser and then do what you do
-            let url = "http://localhost:32629/eventList/" + newTitle;
+            let url = "eventList/" + newTitle;
             $.ajax({
                 url: url,
                 dataType: 'json',
@@ -62,11 +62,10 @@ $(document).ready(function() {
                     for(var i = 0;i<data.length;i++) {
                         $("#calendar-table-contents").append("<tr><td>" + (i+1) +"</th><td>" + data[i]["startDT"]["date"] + "</td><td>" + data[i]["startDT"]["time"] +"</td><td>" + data[i]["summary"] + "</td><td>" + data[i]["numProps"] + "</td><td>" + data[i]["numAlarms"] + "</td></tr>"); // Add the content we will get from the server and parsing
                     }
-
                 }
             });
 
-            var alarmURL = "http://localhost:32629/alarmList/" + newTitle;
+            var alarmURL = "alarmList/" + newTitle;
             $.ajax({
                 url: alarmURL,
                 dataType: 'json',
@@ -81,13 +80,12 @@ $(document).ready(function() {
             });
 
             console.log("calling props");
-            var propURL = "http://localhost:32629/propList/" + newTitle;
+            var propURL = "propList/" + newTitle;
             $.ajax({
                 url: propURL,
                 dataType: 'json',
                 async: false,
                 success: function(data) {
-                    //Now place these into the table
                     $("#event-properties-table-contents").empty();
                     for(var i = 0;i<data.length;i++) {
                         $("#event-properties-table-contents").append("<tr><td>" + data[i]["event"] +"</th><td>" + data[i]["name"] + "</td><td>" + data[i]["description"] +"</td></tr>");
@@ -96,15 +94,11 @@ $(document).ready(function() {
                 }
             });
 
-
-            $("#drop-title").text(newTitle);
-            $(".calendar-table-view").find("div").each(function() {
-                $(this).hide();
+            $("#drop-title").text(newTitle, function() {
+                $(".calendar-table-view").find("#main-event-table").show();
             });
-            $(".calendar-table-view").find("#main-event-table").show();
         });
     });
-
 
     // Find all of the nav tabs and apply an action to them
     $(".nav").find(".nav-item").each(function() {
