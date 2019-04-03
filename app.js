b8636740494587a0c4ec8d57aaef4680989f70f1
James Lemieux <jimmylemieux9@gmail.com>
2019-04-03 14:05:21 -0400

    some push

--- app.js
+++ app.js
@@ -391,43 +391,42 @@ app.get('/dbSaveFiles', function(req, res) {
               console.log("The event table was appended to");
             }
           });
-        }
-      }
-    }
-  });
 
-  //Now I am going to have to get a list of fileEvents
-  connection.query("SELECT * FROM FILE;", function(err, rows, fields) {
-    if(err) {
-      console.log("Something went wrong");
-    } else {
-      //Go through all of the rows in the query
-      var fileNames = [];
+          //Now I am going to have to get a list of fileEvents
+          connection.query("SELECT * FROM FILE;", function(err, rows, fields) {
+            if(err) {
+              console.log("Something went wrong");
+            } else {
+              //Go through all of the rows in the query
+              var fileNames = [];
 
-      for(let row of rows) {
-        var fileName = row.file_Name;
-        fileNames.push(fileName);
+              for(let row of rows) {
+                var fileName = row.file_Name;
+                fileNames.push(fileName);
 
-        var alarmList = sharedLib.alarmJSONWrapper(fileName);
-        var eventList = sharedLib.eventJSONWrapper(fileName);
-        //Go through all of the indi events and put them into the table with reference to the cal_id
+                var alarmList = sharedLib.alarmJSONWrapper(fileName);
+                var eventList = sharedLib.eventJSONWrapper(fileName);
+                //Go through all of the indi events and put them into the table with reference to the cal_id
 
-        var alarmListObj = JSON.parse(alarmList);
-        var eventListObj = JSON.parse(eventList);
+                var alarmListObj = JSON.parse(alarmList);
+                var eventListObj = JSON.parse(eventList);
 
-        for(var i = 0;i<eventListObj.length;i++) { //The event list for each file in the database
-          //Each event will have a specific list of props
-          for(var x = 0;x<alarmListObj.length;x++) {
-            var jsonText = JSON.stringify(alarmListObj[x]);
-            if(alarmListObj[x]["event"] == (i+1)) {
-              console.log("FileName: " + fileName + " alarmJSON " + jsonText);
+                for(var i = 0;i<eventListObj.length;i++) { //The event list for each file in the database
+                  //Each event will have a specific list of props
+                  for(var x = 0;x<alarmListObj.length;x++) {
+                    var jsonText = JSON.stringify(alarmListObj[x]);
+                    if(alarmListObj[x]["event"] == (i+1)) {
+                      console.log("FileName: " + fileName + " alarmJSON " + jsonText);
+                    }
+                  }
+                }
+              }
             }
-          }
+          });
         }
       }
     }
   });
-
 
 
   //Make a query to from the event table 
