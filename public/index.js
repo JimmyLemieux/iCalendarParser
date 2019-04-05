d9387337b2b1263b716be1dfc5dc7734517fe419
James Lemieux <jimmylemieux9@gmail.com>
2019-04-05 14:55:02 -0400

    some push

--- public/index.js
+++ public/index.js
@@ -556,6 +556,7 @@ $(document).ready(function () {
                     var location = data[i].location;
                     var organizer = data[i].organizer;
                     var summary = data[i].summary;
+                    $(".console-output").append("<medium style=\"color:black;\">-----BEGIN EVENT-----</medium></br>");
                     $(".console-output").append("<medium style=\"color:black;\">Event StartTime: " + startTime + ", Event Location: " + location + ", Organizer " + organizer + "Summary: " + summary + "</medium></br>");
                     $(".console-output").append("<medium style=\"color:black;\">-----END EVENT-----</medium></br>");
 
