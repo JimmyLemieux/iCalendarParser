141fae99053c58ad622095296685d2eec1de19a3
James Lemieux <jimmylemieux9@gmail.com>
2019-04-05 16:41:54 -0400

    some push

--- app.js
+++ app.js
@@ -587,7 +587,7 @@ app.get('/getFileEvents', function(req,res) {
 app.get('/getConflictEvents', function(req, res) {
   var arr = [];
 
-  connection.query("SELECT * FROM EVENT GROUP BY start_time HAVING COUNT(*) > 1", function(err, rows, result) {
+  connection.query("SELECT (start_time) FROM EVENT GROUP BY start_time HAVING COUNT(*) > 1", function(err, rows, result) {
     if(err) throw err;
     else {
       for(let row of rows) {
